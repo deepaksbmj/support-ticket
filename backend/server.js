7e940a66fd7c4be8d8dfe7784c1f49b4a53d9c08
deepaksbmj <deepaksingh22431@gmail.com>
2022-06-25 19:56:59 +0530

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,37 @@
-const express = require("express")
-const colors = require("colors")
-const dotEnv = require("dotEnv").config()
-const {errorHandler} = require("./middleware/errorMiddleware")
-const connectDB = require("./config/db")
-const PORT = process.env.PORT || 5000
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotEnv = require("dotEnv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
 
 //Connect to Database
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
-app.get("/", (req,res) => {
-    res.status(200).send({message : "Welcome to Support Desk API"})
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //ROUTES
-app.use("/api/users", require("./routes/userRoutes"))
-app.use("/api/tickets", require("./routes/ticketRoutes"))   
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-app.use(errorHandler)
+//Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  //set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "forntend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).send({ message: "Welcome to Support Desk API" });
+  });
+}
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
